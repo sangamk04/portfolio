a9c3de76f22200c925f33598fab02c4795533ccd
Sangam Kumar Singh <99814514+sangamk04@users.noreply.github.com>
2023-01-02 22:45:11 +0530

    done

---
+++ New folder/index.js
@@ -0,0 +1,7 @@
+ // typing text animation script
+ var typed = new Typed(".typing", {
+    strings: ["HTML", "CSS", "Javascript", "Bootstrap", "Github"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
